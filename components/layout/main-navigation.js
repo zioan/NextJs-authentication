cf929d0777f2387c4012f1d7acc8c141bfd4d1bf
Ioan Zaharia <zaharia.ioan@gmail.com>
2022-06-01 13:49:29 +0200

    basic register, login, log out

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,8 +1,20 @@
 import Link from 'next/link';
+import { useSession, signOut } from 'next-auth/client';
 
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
+  const [session, loading] = useSession();
+
+  //loading is true, and the session (on object ) read the cookie if exists
+  // ones there is a session, the loading is false
+  // console.log(loading);
+  // console.log(session);
+
+  function logOutHandler() {
+    signOut();
+  }
+
   return (
     <header className={classes.header}>
       <Link href='/'>
@@ -12,15 +24,23 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link href='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!session && !loading && (
+            <li>
+              <Link href='/auth'>Login</Link>
+            </li>
+          )}
+
+          {session && (
+            <li>
+              <Link href='/profile'>Profile</Link>
+            </li>
+          )}
+
+          {session && (
+            <li>
+              <button onClick={logOutHandler}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
